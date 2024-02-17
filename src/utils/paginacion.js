661ea3bd0f4084506985e7ccb0e0a47b85e54041
Ares <thiagodev.martins@gmail.com>
2024-02-17 16:56:51 -0300

    Paginacion complete

--- src/utils/paginacion.js
+++ src/utils/paginacion.js
@@ -10,21 +10,26 @@
 
 //SELECT * FROM tabla OFFSET 0 LIMIT 10 ---> Devolverá lo que se encuentre entre OFFSET y LIMIT. En este caso, los primeros 10 elementos.
 
-const resultadosPaginados = async (paginaActual, itemsPorPagina, modeloActual) => {
-    try {
-        const limitAttribute = itemsPorPagina
-        const totalElementos = await modeloActual.count()
-        const totalPaginas = Match.ceil(totalElementos / limitAttribute) //Este parámetro se va a utilizar para manejar apropiadamente los últimos elementos disponibles para visualizar en el Front, por eso no se usa acá y simplemente se retorna.
-        const offseAttribute = (paginaActual - 1) * limitAttribute
+const resultadosPaginados = async (
+	paginaActual,
+	itemsPorPagina,
+	modeloActual
+) => {
+	try {
+		const limitAttribute = itemsPorPagina;
+		const totalElementos = await modeloActual.count();
+		const totalPaginas = Match.ceil(totalElementos / limitAttribute); //Este parámetro se va a utilizar para manejar apropiadamente los últimos elementos disponibles para visualizar en el Front, por eso no se usa acá y simplemente se retorna.
+		const offseAttribute = (paginaActual - 1) * limitAttribute;
 
-        const elementosPaginados = await modeloActual.findAll({
-            offset: offseAttribute,
-            limit: limitAttribute
-        })
+		const elementosPaginados = await modeloActual.findAll({
+			offset: offseAttribute,
+			limit: limitAttribute,
+		});
 
-        return (elementosPaginados, totalPaginas)
-
-    } catch (error) {
-        throw new Error ('Error al obtener los usuarios paginados: ' + error.message)
-    }
-}
+		return { elementosPaginados, totalPaginas };
+	} catch (error) {
+		throw new Error(
+			"Error al obtener los usuarios paginados: " + error.message
+		);
+	}
+};
