69da3fb1f2b7637933819b83ba6a0b9fe0741da8
lisandroviedo <lisandroviedo@hotmail.com>
2024-02-17 18:00:22 -0400

    Agregados los campos creado y actualizado.

--- src/models/Productos_descuentos.js
+++ src/models/Productos_descuentos.js
@@ -1,22 +1,16 @@
 const { DataTypes, UUIDV4 } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define(
-    "Productos_Descuentos",
-    {
-      producto_descuento_id: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        allowNull: false,
-        defaultValue: UUIDV4,
-      },
-      descuento: {
-        type: DataTypes.FLOAT,
-        allowNull: true,
-      },
+  sequelize.define("Productos_Descuentos", {
+    producto_descuento_id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+      defaultValue: UUIDV4,
     },
-    {
-      timestamps: false,
-    }
-  );
+    descuento: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+  });
 };
