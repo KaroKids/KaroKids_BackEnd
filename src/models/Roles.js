513d4c1eec4cb132cda7bdb7cf16ccaa9517f941
lisandroviedo <lisandroviedo@hotmail.com>
2024-02-17 18:04:23 -0400

    Corrección de campo ENUM.
    Agregado del defaultValue del campo rol a "cliente".
    
    Agregados los campos creado y actualizado.

--- src/models/Roles.js
+++ src/models/Roles.js
@@ -1,23 +1,17 @@
 const { DataTypes, UUIDV4 } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define(
-    "Roles",
-    {
-      rol_id: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        allowNull: false,
-        defaultValue: UUIDV4,
-      },
-      rol: {
-        type: DataTypes.ENUM,
-        values: ["Administrador", "Cliente"],
-        allowNull: false,
-      },
+  sequelize.define("Roles", {
+    rol_id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+      defaultValue: UUIDV4,
     },
-    {
-      timestamps: false,
-    }
-  );
+    rol: {
+      type: DataTypes.ENUM("administrador", "cliente"),
+      allowNull: false,
+      defaultValue: "cliente",
+    },
+  });
 };
