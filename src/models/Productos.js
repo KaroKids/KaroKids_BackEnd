5480d0e10aa8fdbf954ea02c4db0e0fd2640d3c9
lisandroviedo <lisandroviedo@hotmail.com>
2024-02-17 18:01:50 -0400

    Agregado campo "genero" para poder filtrar los productos por masculino, femenino o universal.
    Corrección del tipo de dato del campo imagenes_secundarias.
    
    Agregado el defaultValue del campo destacado y del campo inactivo en 0.
    
    Agregados los campos de creado y actualizado.

--- src/models/Productos.js
+++ src/models/Productos.js
@@ -1,51 +1,51 @@
 const { DataTypes, UUIDV4 } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define(
-    "Productos",
-    {
-      producto_id: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        allowNull: false,
-        defaultValue: UUIDV4,
-      },
-      nombre: {
-        type: DataTypes.STRING(50),
-        allowNull: false,
-      },
-      descripcion: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-      },
-      imagen_principal: {
-        type: DataTypes.STRING(200),
-        allowNull: false,
-      },
-      imagenes_secundarias: {
-        type: DataTypes.STRING, //Array de strings
-        allowNull: false,
-      },
-      video: {
-        type: DataTypes.STRING(200),
-        allowNull: false,
-      },
-      precio: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
-        unique: true,
-      },
-      destacado: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-      },
-      inactivo: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-      },
+  sequelize.define("Productos", {
+    producto_id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+      defaultValue: UUIDV4,
     },
-    {
-      timestamps: false,
-    }
-  );
+    nombre: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    descripcion: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    genero: {
+      type: DataTypes.ENUM("masculino", "femenino", "universal"),
+      allowNull: false,
+    },
+    imagen_principal: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    imagenes_secundarias: {
+      type: DataTypes.ARRAY(DataTypes.JSON), //Array de JSONs
+      allowNull: false,
+    },
+    video: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    precio: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      unique: true,
+    },
+    destacado: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: "0",
+    },
+    inactivo: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: "0",
+    },
+  });
 };
