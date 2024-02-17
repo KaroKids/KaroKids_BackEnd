575faed23901d37bb4f8c033ec709e3fe9b99e96
lisandroviedo <lisandroviedo@hotmail.com>
2024-02-17 17:58:28 -0400

    Cambiado el allowNull a true del campo imagen_perfil, para que sea un campo opcional.
    Corrección del límite del campo identificacion.
    
    Corrección de campos ENUM.
    
    Agregados los campos creado y actualizado.

--- src/models/Entidades.js
+++ src/models/Entidades.js
@@ -1,51 +1,44 @@
 const { DataTypes, UUIDV4 } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define(
-    "Entidades",
-    {
-      entidad_id: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        allowNull: false,
-        defaultValue: UUIDV4,
-      },
-      imagen_perfil: {
-        type: DataTypes.STRING(200),
-        allowNull: false, //Añadimos imagen por defecto?
-      },
-      nombres: {
-        type: DataTypes.STRING(50),
-        allowNull: false,
-      },
-      apellidos: {
-        type: DataTypes.STRING(50),
-        allowNull: false,
-      },
-      identificacion: {
-        type: DataTypes.STRING(50),
-        allowNull: false,
-      },
-      fecha_nacimiento: {
-        type: DataTypes.DATEONLY,
-        allowNull: false,
-      },
-      genero: {
-        type: DataTypes.ENUM,
-        values: ["Masculino", "Femenino"],
-        allowNull: false,
-      },
-      telefono_principal: {
-        type: DataTypes.STRING(15),
-        allowNull: false,
-      },
-      telefono_secundario: {
-        type: DataTypes.STRING(15),
-        allowNull: false,
-      },
+  sequelize.define("Entidades", {
+    entidad_id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+      defaultValue: UUIDV4,
     },
-    {
-      timestamps: false,
-    }
-  );
+    imagen_perfil: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    nombres: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    apellidos: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    identificacion: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
+    fecha_nacimiento: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    genero: {
+      type: DataTypes.ENUM("masculino", "femenino"),
+      allowNull: false,
+    },
+    telefono_principal: {
+      type: DataTypes.STRING(15),
+      allowNull: false,
+    },
+    telefono_secundario: {
+      type: DataTypes.STRING(15),
+      allowNull: false,
+    },
+  });
 };
