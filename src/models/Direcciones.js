ed58f0c27e736311aa63c1f196e409f932eb456b
lisandroviedo <lisandroviedo@hotmail.com>
2024-02-17 17:57:13 -0400

    Corrección de campo ENUM.
    Actualización del campo calle a calle_casa (para almacenar ambas cosas en un mismo campo).
    
    Agregados los campos creado y actualizado.

--- src/models/Direcciones.js
+++ src/models/Direcciones.js
@@ -1,43 +1,36 @@
 const { DataTypes, UUIDV4 } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define(
-    "Direcciones",
-    {
-      direccion_id: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        allowNull: false,
-        defaultValue: UUIDV4,
-      },
-      tipo_direccion: {
-        type: DataTypes.ENUM,
-        values: ["Facturación", "Envío"],
-        allowNull: false,
-      },
-      pais: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-      },
-      estado: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-      },
-      ciudad: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-      },
-      calle: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
-      },
-      codigo_postal: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
+  sequelize.define("Direcciones", {
+    direccion_id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+      defaultValue: UUIDV4,
     },
-    {
-      timestamps: false,
-    }
-  );
+    tipo_direccion: {
+      type: DataTypes.ENUM("facturacion", "envio"),
+      allowNull: false,
+    },
+    pais: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    estado: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    ciudad: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    calle_casa: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    codigo_postal: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
 };
