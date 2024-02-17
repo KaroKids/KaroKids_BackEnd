eb611193891d992579a5c33e816013766352289f
lisandroviedo <lisandroviedo@hotmail.com>
2024-02-17 18:02:25 -0400

    Corrección de campos ENUM.
    Agregado el defaultValue del campo inactivo a 0.
    
    Agregados los campos de creado y actualizado.

--- src/models/Promociones.js
+++ src/models/Promociones.js
@@ -1,28 +1,25 @@
-const { DataTypes, UUIDV4 } = require('sequelize');
+const { DataTypes, UUIDV4 } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define('Productos', {
+  sequelize.define("Promociones", {
     promocion_id: {
-      type : DataTypes.UUID,
+      type: DataTypes.UUID,
       primaryKey: true,
       allowNull: false,
-      defaultValue: UUIDV4
+      defaultValue: UUIDV4,
     },
     tipo_media: {
-      type: DataTypes.ENUM,
-      values: ["img_carrusel", "img_banner", "vid_banner"],
-      allowNull: true
+      type: DataTypes.ENUM("img_carrusel", "img_banner", "vid_banner"),
+      allowNull: true,
     },
     url_media: {
       type: DataTypes.STRING,
-      allowNull: true
+      allowNull: true,
     },
     inactivo: {
       type: DataTypes.BOOLEAN,
-      allowNull: true
-    }
-  },
-  {
-    timestamps : false
+      allowNull: true,
+      defaultValue: "0",
+    },
   });
 };
