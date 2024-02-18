1b165a48af0bdb78f4e2b94b0f85774220aeac44
EFDillmann <dillmanncode1999@gmail.com>
2024-02-18 16:44:17 -0300

    filtros y paginacion done

--- src/utils/paginacion.js
+++ src/utils/paginacion.js
@@ -9,61 +9,79 @@
 //OFFSET = (pagina - 1) * LIMIT
 
 //SELECT * FROM tabla OFFSET 0 LIMIT 10 ---> Devolverá lo que se encuentre entre OFFSET y LIMIT. En este caso, los primeros 10 elementos.
-const { Stocks, Colores, Tallas } = require("../db");
+const { Stocks } = require("../db");
 
 const resultadosPaginados = async (
-	paginaActual,
-	itemsPorPagina,
-	modeloActual,
-	filtros
+  paginaActual,
+  itemsPorPagina,
+  modeloActual,
+  whereProducto,
+  whereStock
 ) => {
-	try {
-		const limitAttribute = itemsPorPagina;
-		let totalElementos = await modeloActual.count();
-		let totalPaginas = Math.ceil(totalElementos / limitAttribute); //Este parámetro se va a utilizar para manejar apropiadamente los últimos elementos disponibles para visualizar en el Front, por eso no se usa acá y simplemente se retorna.
-		const offsetAttribute = (paginaActual - 1) * limitAttribute;
+  // try {
+  //   const limitAttribute = itemsPorPagina;
+  //   let totalElementos = await modeloActual.count();
+  //   let totalPaginas = Math.ceil(totalElementos / limitAttribute); //Este parámetro se va a utilizar para manejar apropiadamente los últimos elementos disponibles para visualizar en el Front, por eso no se usa acá y simplemente se retorna.
+  //   const offsetAttribute = (paginaActual - 1) * limitAttribute;
 
-		if (filtros) {
-			const elementosPaginados = await modeloActual.findAll({
-				include: [
-					{
-						model: Stocks,
-						// include: [
-						// 	{ model: Tallas, attributes: ["talla"] },
-						// 	{ model: Colores, attributes: ["color"] },
-						// ],
-					},
-				],
+  //   const elementosPaginados = await modeloActual.findAll({
+  //     include: [
+  //       {
+  //         model: Stocks,
+  //       },
+  //     ],
+  //     offset: offsetAttribute,
+  //     limit: limitAttribute,
+  //   });
 
-				where: filtros,
-				offset: offsetAttribute,
-				limit: limitAttribute,
-			});
-			totalElementos = await modeloActual.count({ where: filtros });
-			totalPaginas = Math.ceil(totalElementos / limitAttribute);
-			return { elementosPaginados, totalPaginas };
-		}
+  //   return { elementosPaginados, totalPaginas };
+  // } catch (error) {
+  //   throw new Error(
+  //     "Error al obtener los usuarios paginados: " + error.message
+  //   );
+  // }
+  try {
+    const limitAttribute = itemsPorPagina;
+    let totalElementos = await modeloActual.count();
+    let totalPaginas = Math.ceil(totalElementos / limitAttribute); //Este parámetro se va a utilizar para manejar apropiadamente los últimos elementos disponibles para visualizar en el Front, por eso no se usa acá y simplemente se retorna.
+    const offsetAttribute = (paginaActual - 1) * limitAttribute;
 
-		const elementosPaginados = await modeloActual.findAll({
-			include: [
-				{
-					model: Stocks,
-					// 	include: [
-					// 		{ model: Tallas, attributes: ["talla"] },
-					// 		{ model: Colores, attributes: ["color"] },
-					// 	],
-				},
-			],
-			offset: offsetAttribute,
-			limit: limitAttribute,
-		});
+    console.log(
+      Object.keys(whereProducto).length,
+      Object.keys(whereStock).length
+    );
+    if (whereProducto) {
+      const filter = await modeloActual.findAll({
+        include: [
+          {
+            model: Stocks,
+            where: whereStock,
+          },
+        ],
+        where: whereProducto,
+        offset: offsetAttribute,
+        limit: limitAttribute,
+      });
 
-		return { elementosPaginados, totalPaginas };
-	} catch (error) {
-		throw new Error(
-			"Error al obtener los usuarios paginados: " + error.message
-		);
-	}
+      totalElementos = await modeloActual.count({
+        include: [{ model: Stocks, where: whereStock }],
+        where: whereProducto,
+      });
+      totalPaginas = Math.ceil(totalElementos / limitAttribute);
+      return { filter, totalPaginas };
+    } else {
+      const elementosPaginados = await modeloActual.findAll({
+        offset: offsetAttribute,
+        limit: limitAttribute,
+      });
+
+      return { elementosPaginados, totalPaginas };
+    }
+  } catch (error) {
+    throw new Error(
+      "Error al obtener los usuarios paginados: " + error.message
+    );
+  }
 };
 
 module.exports = resultadosPaginados;
