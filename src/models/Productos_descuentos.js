04180656cba8785e1d2d564ccbcb7adb83831343
Ares <thiagodev.martins@gmail.com>
2024-02-18 03:04:47 -0300

    Arreglense

--- src/models/Productos_descuentos.js
+++ src/models/Productos_descuentos.js
@@ -1,23 +1,16 @@
 const { DataTypes, UUIDV4 } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define(
-    "Productos_Descuentos",
-    {
-      producto_descuento_id: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        allowNull: false,
-        defaultValue: UUIDV4,
-      },
-      //producto_id: FK
-      descuento: {
-        type: DataTypes.FLOAT,
-        allowNull: true,
-      },
-    },
-    {
-      timestamps: false,
-    }
-  );
+	sequelize.define("Productos_Descuentos", {
+		producto_descuento_id: {
+			type: DataTypes.UUID,
+			primaryKey: true,
+			allowNull: false,
+			defaultValue: UUIDV4,
+		},
+		descuento: {
+			type: DataTypes.FLOAT,
+			allowNull: true,
+		},
+	});
 };
--- src/utils/paginacion.js
+++ src/utils/paginacion.js
@@ -9,19 +9,51 @@
 //OFFSET = (pagina - 1) * LIMIT
 
 //SELECT * FROM tabla OFFSET 0 LIMIT 10 ---> Devolverá lo que se encuentre entre OFFSET y LIMIT. En este caso, los primeros 10 elementos.
+const { Stocks, Colores, Tallas } = require("../db");
 
 const resultadosPaginados = async (
 	paginaActual,
 	itemsPorPagina,
-	modeloActual
+	modeloActual,
+	filtros
 ) => {
 	try {
 		const limitAttribute = itemsPorPagina;
-		const totalElementos = await modeloActual.count();
-		const totalPaginas = Math.ceil(totalElementos / limitAttribute); //Este parámetro se va a utilizar para manejar apropiadamente los últimos elementos disponibles para visualizar en el Front, por eso no se usa acá y simplemente se retorna.
+		let totalElementos = await modeloActual.count();
+		let totalPaginas = Math.ceil(totalElementos / limitAttribute); //Este parámetro se va a utilizar para manejar apropiadamente los últimos elementos disponibles para visualizar en el Front, por eso no se usa acá y simplemente se retorna.
 		const offsetAttribute = (paginaActual - 1) * limitAttribute;
 
+		if (filtros) {
+			const elementosPaginados = await modeloActual.findAll({
+				include: [
+					{
+						model: Stocks,
+						// include: [
+						// 	{ model: Tallas, attributes: ["talla"] },
+						// 	{ model: Colores, attributes: ["color"] },
+						// ],
+					},
+				],
+
+				where: filtros,
+				offset: offsetAttribute,
+				limit: limitAttribute,
+			});
+			totalElementos = await modeloActual.count({ where: filtros });
+			totalPaginas = Math.ceil(totalElementos / limitAttribute);
+			return { elementosPaginados, totalPaginas };
+		}
+
 		const elementosPaginados = await modeloActual.findAll({
+			include: [
+				{
+					model: Stocks,
+					// 	include: [
+					// 		{ model: Tallas, attributes: ["talla"] },
+					// 		{ model: Colores, attributes: ["color"] },
+					// 	],
+				},
+			],
 			offset: offsetAttribute,
 			limit: limitAttribute,
 		});
