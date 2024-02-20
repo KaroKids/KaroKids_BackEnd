450bd27d60ab4d3d3ecb9959d4d623501d4e9d09
JGerFuentes <126228528+JGerFuentes@users.noreply.github.com>
2024-02-20 20:34:57 -0300

    Update productosControllers.js

--- src/controllers/productosControllers.js
+++ src/controllers/productosControllers.js
@@ -3,17 +3,17 @@
 const { Productos } = require("../db");
 const resultadosPaginados = require("../utils/paginacion");
 
-//* Requerimos las credenciales del archivo .env y definimos la configuración de Cloudinary.
-require("dotenv").config();
-const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true,
-});
-//*
+  // //* Requerimos las credenciales del archivo .env y definimos la configuración de Cloudinary.
+  // require("dotenv").config();
+  // const cloudinary = require("cloudinary").v2;
+  
+  // cloudinary.config({
+  //   cloud_name: process.env.CLOUDINARY_NAME,
+  //   api_key: process.env.CLOUDINARY_API_KEY,
+  //   api_secret: process.env.CLOUDINARY_API_SECRET,
+  //   secure: true,
+  // });
+  // //*
 
 const todosLosProductos = async (paginaActual) => {
   const itemsPorPagina = 12;
@@ -58,25 +58,25 @@ const crearProducto = async (
   inactivo,
   stock
 ) => {
-  try {
-    // Subimos la imagen principal a Cloudinary
-    const result_principal = await cloudinary.uploader.upload(imagen_principal);
-    const imagen_principal_url = result_principal.secure_url;
+  // try {
+    // // Subimos la imagen principal a Cloudinary
+    // const result_principal = await cloudinary.uploader.upload(imagen_principal);
+    // const imagen_principal_url = result_principal.secure_url;
 
-    // Subimos las imágenes secundarias a Cloudinary
-    const imagenes_secundarias_urls = [];
-    for (const imagen of imagenes_secundarias) {
-      const result_secundaria = await cloudinary.uploader.upload(imagen);
-      imagenes_secundarias_urls.push(result_secundaria.secure_url);
-    }
+    // // Subimos las imágenes secundarias a Cloudinary
+    // const imagenes_secundarias_urls = [];
+    // for (const imagen of imagenes_secundarias) {
+    //   const result_secundaria = await cloudinary.uploader.upload(imagen);
+    //   imagenes_secundarias_urls.push(result_secundaria.secure_url);
+    // }
 
     return await Productos.create({
       nombre,
       descripcion,
-      // imagen_principal,
-      // imagenes_secundarias,
-      imagen_principal: imagen_principal_url,
-      imagenes_secundarias: imagenes_secundarias_urls,
+      imagen_principal,
+      imagenes_secundarias,
+      // imagen_principal: imagen_principal_url,
+      // imagenes_secundarias: imagenes_secundarias_urls,
       video,
       edad,
       genero,
@@ -85,10 +85,10 @@ const crearProducto = async (
       inactivo,
       stock,
     });
-  } catch (error) {
-    console.error("Error al cargar la imagen", error);
-    throw new Error("Error al cargar la imagen");
-  }
+  // } catch (error) {
+  //   console.error("Error al cargar la imagen", error);
+  //   throw new Error("Error al cargar la imagen");
+  // }
 };
 
 const filtrarProductos = async () => {};
