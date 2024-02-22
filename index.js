920e606a550f503278cd4a0f0009a7c9551520d4
KaroKids <158864281+KaroKids@users.noreply.github.com>
2024-02-22 17:40:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { conn } = require("./src/db.js");
 const PORT = process.env.PORT || 3001;
 
 conn
-  .sync({ force: false })
+  .sync({ alter: true })
   .then(() => {
     server.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`);
