f31e398e02965edcbdfadc79ed58bfeba3b3978c
lisandroviedo <lisandroviedo@hotmail.com>
2024-02-22 20:44:26 -0400

    Se eliminó el campo productos, para luego relacionar la tabla con la nueva tabla Carritos_Detalles

--- src/models/Carritos.js
+++ src/models/Carritos.js
@@ -1,28 +1,23 @@
 const { DataTypes, UUIDV4 } = require("sequelize");
 
 module.exports = (sequelize) => {
-	sequelize.define(
-		"Carritos",
-		{
-			carrito_id: {
-				type: DataTypes.UUID,
-				primaryKey: true,
-				allowNull: false,
-				defaultValue: UUIDV4,
-			},
-			//usuario_id: FK
-			productos: {
-				type: DataTypes.ARRAY(DataTypes.JSON), //Array de JSONs
-				allowNull: false,
-			},
-			inactivo: {
-				type: DataTypes.BOOLEAN,
-				allowNull: false,
-				defaultValue: "0",
-			},
-		},
-		{
-			timestamps: false,
-		}
-	);
+  sequelize.define(
+    "Carritos",
+    {
+      carrito_id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: UUIDV4,
+      },
+      inactivo: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: "0",
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
 };
