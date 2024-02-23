52db286015a20e377aec32f8a80232f4d10bfe5b
lisandroviedo <lisandroviedo@hotmail.com>
2024-02-22 20:43:29 -0400

    Primera versión de carritosHandlers

--- src/handlers/carritosHandlers.js
+++ src/handlers/carritosHandlers.js
@@ -1,62 +1,69 @@
-const { todosLosCarritos, traerCarrito, crearCarrito } = require("../controllers/carritosController");
-
+const {
+  todosLosCarritos,
+  traerCarrito,
+  borrarCarrito,
+  modificarCarrito,
+  crearCarrito,
+} = require("../controllers/carritosController");
 
 const getCarritos = async (req, res) => {
-    try{
-        const response = await todosLosCarritos();
-        res.status(200).json(response);
-    }
-    catch(error){
-        res.status(400).json({error: error.message});
-    }
-}
+  try {
+    const response = await todosLosCarritos();
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 const getCarrito = async (req, res) => {
-    const {id} = req.params;
-    try{
-        const response = await traerCarrito(id);
-        res.status(200).json(response);
-    }
-    catch(error){
-        res.status(400).json({error: error.message});
-    }
-}
+  const { usuario_id } = req.params;
+  try {
+    const response = await traerCarrito(usuario_id);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 const postCarrito = async (req, res) => {
-    const {a} = req.body;
-    try{
-        const response = await crearCarrito(a)
-        res.status(200).send('se creo el Carrito');
-    }
-    catch(error){
-        res.status(400).json({error: error.message});
-    }
-}
+  const { usuario_id } = req.body;
+
+  try {
+    const response = await crearCarrito(usuario_id);
+
+    res.status(200).send("se creo el Carrito");
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 const putCarrito = async (req, res) => {
-    const {id} = req.params;
-    try{
-        res.status(200).send(`se modifico el Carrito ${id}`);
-    }
-    catch(error){
-        res.status(400).json({error: error.message});
-    }
-}
+  const { carrito_id } = req.params;
+
+  try {
+    const response = await modificarCarrito(carrito_id);
+    res.status(200).send(`se modifico el Carrito ${id}`);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 const deleteCarrito = async (req, res) => {
-    const {id} = req.params;
-    try{
-        res.status(200).send(`se elimino el Carrito ${id}`);
-    }
-    catch(error){
-        res.status(400).json({error: error.message});
-    }
-}
+  const { usuario_id } = req.params;
+
+  try {
+    const response = await borrarCarrito(usuario_id);
+
+    res.status(200).send(`se elimino el Carrito ${id}`);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 module.exports = {
-    getCarritos,
-    getCarrito,
-    postCarrito,
-    putCarrito,
-    deleteCarrito
-}
+  getCarritos,
+  getCarrito,
+  postCarrito,
+  putCarrito,
+  deleteCarrito,
+};
