82c0c1f763b07a3b9f09e4ddeb28c07a22a04de1
lisandroviedo <lisandroviedo@hotmail.com>
2024-02-23 22:58:05 -0400

    Actualizadas las rutas para carritos

--- src/routes/carritos.js
+++ src/routes/carritos.js
@@ -1,11 +1,15 @@
-const {Router} = require ('express');
-const { getCarritos, getCarrito, postCarrito, putCarrito, deleteCarrito } = require('../handlers/carritosHandlers');
+const { Router } = require("express");
+const {
+  getCarrito,
+  postCarrito,
+  putCarrito,
+  deleteCarrito,
+} = require("../handlers/carritosHandlers");
 const carritos = Router();
 
-carritos.get('/',getCarritos)
-carritos.get('/:id',getCarrito)
-carritos.post('/',postCarrito)
-carritos.put('/:id',putCarrito)
-carritos.delete('/:id',deleteCarrito)
+carritos.get("/:usuarioId", getCarrito);
+carritos.post("/:usuarioId", postCarrito);
+carritos.put("/:carritoId", putCarrito);
+carritos.delete("/:carritoId", deleteCarrito);
 
 module.exports = carritos;
