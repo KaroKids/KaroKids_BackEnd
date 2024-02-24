4c8015f9bc8e44a4d70b975d199480dd562ae139
lisandroviedo <lisandroviedo@hotmail.com>
2024-02-23 23:03:26 -0400

    Actualizadas las rutas de productos favoritos

--- src/routes/productos_favoritos.js
+++ src/routes/productos_favoritos.js
@@ -1,11 +1,13 @@
-const {Router} = require ('express');
-const { getProductos_favoritos, getProducto_favorito, postProducto_favorito, putProducto_favorito, deleteProducto_favorito } = require('../handlers/productos_favoritosHandlers');
+const { Router } = require("express");
+const {
+  getProductosFavoritos,
+  addProductoFavorito,
+  deleteProductoFavorito,
+} = require("../handlers/productosFavoritosHandlers");
 const productos_favoritos = Router();
 
-productos_favoritos.get('/',getProductos_favoritos)
-productos_favoritos.get('/:id',getProducto_favorito)
-productos_favoritos.post('/',postProducto_favorito)
-productos_favoritos.put('/:id',putProducto_favorito)
-productos_favoritos.delete('/:id',deleteProducto_favorito)
+productos_favoritos.get("/", getProductosFavoritos);
+productos_favoritos.post("/", addProductoFavorito);
+productos_favoritos.delete("/", deleteProductoFavorito);
 
 module.exports = productos_favoritos;
