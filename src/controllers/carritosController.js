98b432b9bd6dbed227ece40b4bd024aa658367dc
Ares <thiagodev.martins@gmail.com>
2024-02-26 03:09:31 -0300

    Se borro el findOne con el inactivo

--- src/controllers/carritosController.js
+++ src/controllers/carritosController.js
@@ -5,7 +5,7 @@ const { agregarProducto } = require("../utils/metodosProductos"); //Traemos de "
 const traerCarrito = async (usuario_id) => {
 	try {
 		let carritoUsuario = await Carritos.findOne({
-			where: [{ usuario_id: usuario_id }, { inactivo: false }],
+			where: [{ usuario_id: usuario_id }],
 		});
 
 		if (!carritoUsuario) {
@@ -14,6 +14,7 @@ const traerCarrito = async (usuario_id) => {
 
 		return carritoUsuario;
 	} catch (error) {
+		console.log(error);
 		throw new Error("Aún no ha creado un carrito de compra", error);
 	}
 };
@@ -52,54 +53,54 @@ const crearCarrito = async (
 };
 
 const actualizarCarrito = async (usuario_id, producto_id) => {
-  //Permite actualizar el carrito luego de la eliminación de un producto.
-  try {
-    const carritoUsuario = await eliminarProducto(usuario_id, producto_id);
+	//Permite actualizar el carrito luego de la eliminación de un producto.
+	try {
+		const carritoUsuario = await eliminarProducto(usuario_id, producto_id);
 
-    return carritoUsuario;
-  } catch (error) {
-    throw new Error("No se pudo actualizar el carrito");
-  }
+		return carritoUsuario;
+	} catch (error) {
+		throw new Error("No se pudo actualizar el carrito");
+	}
 };
 
 const borrarProductoCarrito = async (
-  usuario_id,
-  producto_id,
-  compra_talla,
-  compra_color
+	usuario_id,
+	producto_id,
+	compra_talla,
+	compra_color
 ) => {
-  try {
-    // Obtener el carrito del usuario
-    let carritoUsuario = await Carritos.findOne({
-      where: [{ usuario_id: usuario_id }, { inactivo: false }],
-    });
+	try {
+		// Obtener el carrito del usuario
+		let carritoUsuario = await Carritos.findOne({
+			where: [{ usuario_id: usuario_id }, { inactivo: false }],
+		});
 
-    console.log("Antiguos productos: ", carritoUsuario);
+		console.log("Antiguos productos: ", carritoUsuario);
 
-    if (carritoUsuario) {
-      // Filtrar el array productos_compra para eliminar el producto con el producto_id dado
-      const nuevosProductos = carritoUsuario.productos_compra.filter(
-        (producto) =>
-          producto.producto_id !== producto_id ||
-          producto.compra_talla !== compra_talla ||
-          producto.compra_color !== compra_color
-      );
+		if (carritoUsuario) {
+			// Filtrar el array productos_compra para eliminar el producto con el producto_id dado
+			const nuevosProductos = carritoUsuario.productos_compra.filter(
+				(producto) =>
+					producto.producto_id !== producto_id ||
+					producto.compra_talla !== compra_talla ||
+					producto.compra_color !== compra_color
+			);
 
-      // Actualizar el carrito con los nuevos productos
-      await carritoUsuario.update({ productos_compra: nuevosProductos });
+			// Actualizar el carrito con los nuevos productos
+			await carritoUsuario.update({ productos_compra: nuevosProductos });
 
-      return carritoUsuario;
-    } else {
-      throw new Error("No se encontró el carrito especificado");
-    }
-  } catch (error) {
-    throw new Error("No se pudo eliminar el producto del carrito", error);
-  }
+			return carritoUsuario;
+		} else {
+			throw new Error("No se encontró el carrito especificado");
+		}
+	} catch (error) {
+		throw new Error("No se pudo eliminar el producto del carrito", error);
+	}
 };
 
 module.exports = {
-  traerCarrito,
-  crearCarrito,
-  actualizarCarrito,
-  borrarProductoCarrito,
+	traerCarrito,
+	crearCarrito,
+	actualizarCarrito,
+	borrarProductoCarrito,
 };
