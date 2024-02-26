19adf244ece10b55a844983e0ab77d0b2d02c2c2
lisandroviedo <lisandroviedo@hotmail.com>
2024-02-25 23:24:16 -0400

    Añadida la posibilidad de eliminar un producto y añadida la validación de no agregar productos repetidos

--- src/utils/metodosProductos.js
+++ src/utils/metodosProductos.js
@@ -23,6 +23,17 @@ const agregarProducto = async (
     // Se obtiene el valor actual de "productos_compra" del carrito
     let productosCompraActual = carritoUsuario.productos_compra || [];
     // Se agrega el nuevo objeto al arreglo
+
+    productosCompraActual.forEach((producto) => {
+      if (
+        producto.producto_id === producto_id &&
+        producto.compra_talla === compra_talla &&
+        producto.compra_color === compra_color
+      ) {
+        throw new Error("El producto ya está en el carrito");
+      }
+    });
+
     productosCompraActual.push(nuevaCompra);
 
     // Se realiza la actualización utilizando el método update de Sequelize
@@ -38,36 +49,14 @@ const agregarProducto = async (
 
     return carritoActualizado;
   } catch (error) {
-    throw new Error("Error al agregar el producto al carrito: ", error);
-  }
-};
-
-const eliminarProducto = async (carrito_id, producto_id) => {
-  try {
-    // Obtener el carrito del usuario
-    const carrito = await Carritos.findByPk(carrito_id, {
-      where: { inactivo: 0 },
-    });
-
-    if (carrito) {
-      // Filtrar el array productos_compra para eliminar el producto con el producto_id dado
-      const nuevosProductos = carrito.productos_compra.filter(
-        (producto) => producto.producto_id !== producto_id
-      );
-
-      // Actualizar el carrito con los nuevos productos
-      await carrito.update({ productos_compra: nuevosProductos });
-
-      return carrito;
+    if (error.message === "El producto ya está en el carrito") {
+      throw new Error("El producto ya está en el carrito");
     } else {
-      throw new Error("No se encontró el carrito especificado");
+      throw new Error("Error al agregar el producto al carrito: ", error);
     }
-  } catch (error) {
-    throw new Error("No se pudo eliminar el producto del carrito", error);
   }
 };
 
 module.exports = {
   agregarProducto,
-  eliminarProducto,
 };
