8ee2c4ccf275aed2a7264c0d8619c798ebdf8ae0
EFDillmann <dillmanncode1999@gmail.com>
2024-02-27 20:19:07 -0300

    pasarela con redireccionamiento

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -5,6 +5,10 @@ const client = new MercadoPagoConfig({
     "TEST-3395648537928946-022217-68a657898d160b4ebe5d2032f1329091-1694080585",
 });
 
+const success = async (req, res) => {
+  res.redirect("https://karokids-frontend.vercel.app/productos");
+};
+
 const createOrder = async (req, res) => {
   const body = {
     items: [
@@ -16,11 +20,15 @@ const createOrder = async (req, res) => {
       },
     ],
     back_urls: {
-      success: "https://karokids.onrender.com/payment/success",
-      failure: "https://karokids.onrender.com/payment/failure",
-      pending: "https://karokids.onrender.com/payment/pending",
+      success:
+        "https://c737-2800-810-4ff-12af-a4f9-58b5-e4b5-509a.ngrok-free.app/payment/success",
+      failure:
+        "https://c737-2800-810-4ff-12af-a4f9-58b5-e4b5-509a.ngrok-free.app/payment/failure",
+      pending:
+        "https://c737-2800-810-4ff-12af-a4f9-58b5-e4b5-509a.ngrok-free.app/payment/pending",
     },
-    notification_url: "https://karokids.onrender.com/payment/webhook",
+    notification_url:
+      "https://c737-2800-810-4ff-12af-a4f9-58b5-e4b5-509a.ngrok-free.app/payment/webhook",
     auto_return: "approved",
   };
   const preference = new Preference(client);
@@ -47,4 +55,4 @@ const receiveWebhook = async (req, res) => {
   }
 };
 
-module.exports = { createOrder, receiveWebhook };
+module.exports = { createOrder, receiveWebhook, success };
--- src/routes/payment.js
+++ src/routes/payment.js
@@ -2,12 +2,13 @@ const { Router } = require("express");
 const {
   createOrder,
   receiveWebhook,
+  success,
 } = require("../controllers/paymentController");
 
 const payment = Router();
 
 payment.post("/create-order", createOrder);
-payment.get("/success", (req, res) => res.send("success"));
+payment.get("/success", success);
 payment.get("/failure", (req, res) => res.send("failure"));
 payment.get("/pending", (req, res) => res.send("pending"));
 payment.post("/webhook", receiveWebhook);
