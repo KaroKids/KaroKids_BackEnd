647bda6e5ce62c4db57dc909c3d43f31c4862376
Lisandro <lisandroviedo@hotmail.com>
2024-02-27 11:41:21 -0400

    Añadida validación al activar / desactivar producto
    Ahora retorna el producto con el cambio realizado

--- src/handlers/productosHandlers.js
+++ src/handlers/productosHandlers.js
@@ -1,94 +1,91 @@
 const {
-	crearProducto,
-	todosLosProductos,
-	traerProducto,
-	borrarProducto,
+  crearProducto,
+  todosLosProductos,
+  traerProducto,
+  borrarProducto,
 } = require("../controllers/productosControllers");
 
 const getProductos = async (req, res) => {
-	const { paginaActual } = req.query;
-	if (!paginaActual) {
-		paginaActual = 1;
-	}
-	console.log(paginaActual);
-	try {
-		const response = await todosLosProductos(paginaActual);
-		res.status(200).json(response);
-	} catch (error) {
-		res.status(400).json({ error: error.message });
-	}
+  const { paginaActual } = req.query;
+  if (!paginaActual) {
+    paginaActual = 1;
+  }
+
+  try {
+    const response = await todosLosProductos(paginaActual);
+    res.json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 const getProducto = async (req, res) => {
-	const { id } = req.params;
-	try {
-		const response = await traerProducto(id);
-		res.status(200).json(response);
-	} catch (error) {
-		res.status(400).json({ error: error.message });
-	}
+  const { id } = req.params;
+  try {
+    const response = await traerProducto(id);
+    res.json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 const postProducto = async (req, res) => {
-	const {
-		nombre,
-		descripcion,
-		imagen_principal,
-		imagenes_secundarias,
-		edad,
-		genero,
-		precio,
-		destacado,
-		inactivo,
-		stock,
-	} = req.body;
-	try {
-		const response = await crearProducto(
-			nombre,
-			descripcion,
-			imagen_principal,
-			imagenes_secundarias,
-			edad,
-			genero,
-			precio,
-			destacado,
-			inactivo,
-			stock
-		);
+  const {
+    nombre,
+    descripcion,
+    imagen_principal,
+    imagenes_secundarias,
+    edad,
+    genero,
+    precio,
+    destacado,
+    inactivo,
+    stock,
+  } = req.body;
+  try {
+    const response = await crearProducto(
+      nombre,
+      descripcion,
+      imagen_principal,
+      imagenes_secundarias,
+      edad,
+      genero,
+      precio,
+      destacado,
+      inactivo,
+      stock
+    );
 
-		res.status(200).json(response);
-	} catch (error) {
-		res.status(400).json({ error: error.message });
-	}
+    res.json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 const putProducto = async (req, res) => {
-	const { id } = req.params;
-	try {
-		res.status(200).send(`se modifico el producto ${id}`);
-	} catch (error) {
-		res.status(400).json({ error: error.message });
-	}
+  const { id } = req.params;
+  try {
+    res.send(`se modifico el producto ${id}`);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 const deleteProducto = async (req, res) => {
-	const { producto_id } = req.body;
-	try {
-		const response = await borrarProducto(producto_id);
-		if (response === true) {
-			res.status(200).send(`se elimino el producto ${producto_id}`);
-		} else {
-			res.status(200).send(`se activo el producto ${producto_id}`);
-		}
-	} catch (error) {
-		res.status(400).json({ error: error.message });
-	}
+  const { producto_id } = req.body;
+  try {
+    const response = await borrarProducto(producto_id);
+
+    res.json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = {
-	getProductos,
-	getProducto,
-	postProducto,
-	putProducto,
-	deleteProducto,
+  getProductos,
+  getProducto,
+  postProducto,
+  putProducto,
+  deleteProducto,
 };
