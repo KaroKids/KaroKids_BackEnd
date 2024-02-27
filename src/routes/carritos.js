9ed325dc606e61b34308318fe6f48bb6ac82fc7e
JGerFuentes <jgerfuentes@gmail.com>
2024-02-27 10:24:25 -0300

    Se modificó la ruta de los métodos GET y DELETE, para mantener la coherencia en la denominación de los parámetros.

--- src/routes/carritos.js
+++ src/routes/carritos.js
@@ -8,10 +8,10 @@ const {
 } = require("../handlers/carritosHandlers");
 const carritos = Router();
 
-carritos.get("/:id", getCarrito); //carritos/c77e5271-7c45-4b8e-aa5b-55e8ff309d38
+carritos.get("/:usuario_id", getCarrito); //carritos/c77e5271-7c45-4b8e-aa5b-55e8ff309d38
 carritos.put('/agregar', addProducto)
 carritos.put('/eliminar', deleteProducto)
 carritos.put('/modificar', updateProducto)
-carritos.delete("/:id", deleteCarrito); //carritos/c77e5271-7c45-4b8e-aa5b-55e8ff309d38
+carritos.delete("/:usuario_id", deleteCarrito); //carritos/c77e5271-7c45-4b8e-aa5b-55e8ff309d38
 
 module.exports = carritos;
