c985f837d7329943ed7c5585b16b0d4b205cc7aa
KaroKids <158864281+KaroKids@users.noreply.github.com>
2024-02-27 18:23:24 -0300

    update limits

--- src/server.js
+++ src/server.js
@@ -8,9 +8,9 @@ const server = express();
 
 server.use(cors());
 server.use(morgan("dev"));
-server.use(express.json());
-server.use(express.json({limit: '650mb'})); //Límite máximo en el tamaño de los datos JSON que el servidor puede manejar de una sola vez, para evitar posibles ataques de denegación de servicio (DoS) o abusos.
-server.use(express.urlencoded({extended: true, limit: '650mb'})); //Cuando envías datos desde un formulario HTML, estos datos generalmente se envían en formato URL codificado. Este middleware analiza estos datos y los convierte en un objeto JavaScript accesible a través de req.body. El parámetro {extended: true} permite el análisis de datos codificados en la URL con formato complejo (por ejemplo, datos anidados) en lugar de solo datos simples.
+//server.use(express.json());
+server.use(express.json({limit: '65mb'})); //Límite máximo en el tamaño de los datos JSON que el servidor puede manejar de una sola vez, para evitar posibles ataques de denegación de servicio (DoS) o abusos.
+server.use(express.urlencoded({extended: true, limit: '65mb'})); //Cuando envías datos desde un formulario HTML, estos datos generalmente se envían en formato URL codificado. Este middleware analiza estos datos y los convierte en un objeto JavaScript accesible a través de req.body. El parámetro {extended: true} permite el análisis de datos codificados en la URL con formato complejo (por ejemplo, datos anidados) en lugar de solo datos simples.
 
 server.use(router);
 
