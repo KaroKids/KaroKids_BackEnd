6da0c19a67cc9b0d85756cf51c05daf273eeaa25
lisandroviedo <lisandroviedo@hotmail.com>
2024-03-03 10:56:23 -0400

    Agregada ruta para productos handlers
    Actualizada ruta de detalle del producto

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -5,12 +5,14 @@ const {
   deleteProducto,
   postProducto,
   standOutProducto,
+  getProductosDestacados,
 } = require("../handlers/productosHandlers");
 const productosFiltrados = require("../controllers/productosFiltradosController");
 const productos = Router();
 
 productos.get("/", productosFiltrados);
-productos.get("/:id", getProducto);
+productos.get("/detalle/:id", getProducto);
+productos.get("/destacados", getProductosDestacados);
 productos.post("/", postProducto);
 productos.put("/modificar", putProducto);
 productos.put("/", deleteProducto);
